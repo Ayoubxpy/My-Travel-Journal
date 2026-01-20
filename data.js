c29081e63e5ac4d74eea940993c692af18e92efb
Ayoub <84749143+Ayoubxpy@users.noreply.github.com>
2026-01-20 21:21:55 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "./images/marakkech.jpg",
+            alt: "Jemaa el-Fnaa, Marrakech"
+        },
+        title: "Jemaa el-Fnaa",
+        country: "Morocco",
+        googleMapsLink: "https://maps.app.goo.gl/2YNfVD6AvuAcERnr9",
+        dates: "Last visit in : 2024",
+        text: "Jemaa el-Fnaa is the main square in Marrakech and one of the most vibrant places in Morocco. It is famous for its street performers, food stalls, musicians, and traditional atmosphere."
+    },
+    {
+        id: 2,
+        img: {
+            src: "./images/casablanca.jpg",
+            alt: "Hassan II Mosque, Casablanca"
+        },
+        title: "Hassan II Mosque",
+        country: "Morocco",
+        googleMapsLink: "https://maps.app.goo.gl/dfC5B5UbFS6oCqAN6",
+        dates: "Last Visit in : 2023",
+        text: "The Hassan II Mosque is one of the largest mosques in the world, located in Casablanca along the Atlantic Ocean. It is known for its impressive architecture and the tallest minaret on Earth."
+    },
+    {
+        id: 3,
+        img: {
+            src: "./images/safi.jpg",
+            alt: "Safi Coast"
+        },
+        title: "Safi Coast",
+        country: "Morocco",
+        googleMapsLink: "https://maps.app.goo.gl/8S3LR4U4sU3zC88f9",
+        dates: "My Home Town",
+        text: "Safi is a coastal city in Morocco known for its pottery, fishing port, and strong surfing culture. The city combines historical heritage with a calm seaside atmosphere."
+    }
+]
